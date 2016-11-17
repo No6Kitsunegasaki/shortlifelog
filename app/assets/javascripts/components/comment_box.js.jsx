00075e55fc9b3d661a6795ca7e88b6262decf1ad
No6Kitsunegasaki <none.gingerale@gmail.com>
2016-11-17 11:52:38 +0900

    from api

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -1,10 +1,76 @@
 var CommentBox = React.createClass({
 
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(result) {
+        this.setState({data: result.data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
-      <div className='commentBox'>
-        hi...
+      <div className="commentBox">
+        <h2>comment</h2>
+        <CommentList data={this.state.data} />
+        <CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment key={comment.id} author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+
+  render: function() {
+    return (
+      <div className="commentForm">
+        form...
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+
+  render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      <div className="comment">
+        <div className="author">
+          {this.props.author}
+        </div>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'notes/write'
 
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :comments
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
