2171bda91fab5557d8f1baaefc3b2718f06c0b9e
No6Kitsunegasaki <none.gingerale@gmail.com>
2016-11-17 12:26:29 +0900

    chage name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_shortlifelog_session'
+Rails.application.config.session_store :cookie_store, key: '_text_recoder_session'
